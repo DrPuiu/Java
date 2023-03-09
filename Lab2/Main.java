59ca244938f073839e7e18d9b4fedf54ff3ff669
Puiu Andrei <66256514+DrPuiu@users.noreply.github.com>
2023-03-09 09:56:45 +0200

    Update Main.java

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -2,6 +2,7 @@ import java.sql.SQLOutput;
 
 public class    Main {
     public static void main(String[] args) {
+        //initializam 2 locatii si un drum iar apoi le afisam
         Location location1= new Location("location1", 50, 50, LocationType.CITY);
         Location location2= new Location("location1", 60, 60, LocationType.CITY);
         Road road1 = new Road(50, 90, location1, location2, RoadType.EXPRESS );
